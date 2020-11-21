6e4a9e50356a01ff0ad340c42981ae92b328d42e
aburlaka2 <aburlaka1@gmail.com>
2020-11-21 16:22:29 +0200

    Update Program.cs

--- Homework5App/Program.cs
+++ Homework5App/Program.cs
@@ -138,8 +138,8 @@ namespace Homework5App
         {
             for(int i=1; size>i; i++)
             {
-                var key = array[i];
-                var j = i;
+                int key = array[i];
+                int j = i;
                 while ((j > 1) && (array[j - 1] > key))
                 {
                     int temp = array[j - 1];
